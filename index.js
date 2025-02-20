a6cdc9eec2e0dc5badd9574a78af279b89ca6071
tanver909 <tanveermahidi@gmial.com>
2025-02-20 06:51:48 +0600

    server setup and mongodb installed

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Parse JSON body
+
+// Connect to MongoDB
+
+// Test API Route
+app.get("/", (req, res) => {
+    res.send("Task Management API is running..");
+});
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
